55e4924db39d143bf5dd6e3c4e260b0c0a37e9f6
deepa0101 <jaysrideepa@gmail.com>
2024-02-22 22:19:18 +0530

    first

---
+++ form_dom/script.js
@@ -0,0 +1,30 @@
+function addTableRow(){
+    let loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+    })
+
+let firstName = document.getElementById('fName').value;
+let lastName = document.getElementById('lName').value;
+let Address = document.getElementById('add').value;
+let Pincode = document.getElementById('pincode').value;
+let gender = document.getElementById('gender').value;
+let fc1 = document.getElementById("item1").value;
+let fc2 = document.getElementById("item2").value;
+let state = document.getElementById("state").value;
+let country = document.getElementById("ctry").value;
+
+let a = [firstName,lastName,Address,Pincode,gender,fc1,fc2,state,country];
+var table = document.getElementById('table')
+var tbl = document.getElementById('tbody')
+var row = document.createElement("tr")
+for (var i=0;i<a.length;i++){
+    var cell=document.createElement("td");
+    cell.innerText = a[i];
+    row.appendChild(cell)
+}
+tbl.appendChild(row)
+table.appendChild(tbl)
+document.body.appendChild(table);
+loginForm.reset();
+}
